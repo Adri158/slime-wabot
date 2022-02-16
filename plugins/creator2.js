b9a26c36d3a60c2659bfed9d1758295d90fd6553
Adri <99004389+Adri158@users.noreply.github.com>
2022-02-17 00:24:08 +0800

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 
     "displayName": "OWNER•SLIME",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:OWNER•SLIME\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Owner Real2\nitem2.EMAIL;type=INTERNET:slimebotz011@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:OWNER•SLIME\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:slimebotz011@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ By WhatsApp ]───────\nEND:VCARD"
