21f1a26d8a7d6423ce77542877aeeb4359c86daa
Adri <99004389+Adri158@users.noreply.github.com>
2022-02-16 23:36:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ function pickRandom(list) {
 
 global.DeveloperMode = 'false' //Buat Mode True False Moderator
 global.linkGC = ['https://chat.whatsapp.com/F9dPQcOf82Z4Dc9uxE7Kh0'] // ganti jadi group lu
-global.owner = ['6281354079196', '6287735559197', '6287774166385'] // Ganti jadi nomor lu ajg nanti ngechat bot lu pas sv database
+global.owner = ['6281354079196'] // Ganti jadi nomor lu ajg nanti ngechat bot lu pas sv database
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit
 global.APIs = { // API Prefix
