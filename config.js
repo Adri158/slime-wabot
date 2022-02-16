74b8638bf076f6ece5c7e0fb27855cd8811bfb7c
Adri <99004389+Adri158@users.noreply.github.com>
2022-02-16 23:37:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@
 // thank you to zerochanBot
 // thank you to fernazer
 // thank you to MikeBot Dev Team
+// thank you to kannachann
 // thank you to ALL Bot creator
 // and thanks you to who support my Bot
 let fs = require('fs')
